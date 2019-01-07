c33a4bf4453b945b1fbd11359ddc0b8039b95b61
Mike Hendriks <info@mikehendriks.nl>
2019-01-07 15:44:54 +0100

    First styled prototype created

--- index.php
+++ index.php
@@ -4,12 +4,26 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Document</title>
+    <title>Interactive poster - 001</title>
     <link rel="stylesheet" href="inc/css/style.css" />
   </head>
   <body id="body">
     <main>
-      <div class="top"><h1>Canon EOS 650D</h1></div>
+
+      <div class="right_left">
+          <img src="inc/img/icons/instagram.svg" alt="instagram icon"/>
+          mike.hendriks_
+      </div>
+      
+      <div class="right_top"> 
+        <span>20</span>
+        <span>19</span>
+      </div>
+
+      <div class="left_bottom"> 
+        Poster
+        <span>001</span>
+      </div>
     </main>
     <script src="inc/js/main.js"></script>
   </body>
