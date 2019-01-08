95bfe5224d8062f80b7752d06d5fe10c6e6cadb9
Mike Hendriks <info@mikehendriks.nl>
2019-01-09 00:27:45 +0100

    Send data from Arduino & censor to browser and log it in the console

--- inc/js/main.js
+++ inc/js/main.js
@@ -1,57 +1,60 @@
-(function() {
-  document.onmousemove = handleMouseMove;
-  function handleMouseMove(event) {
-    var dot, eventDoc, doc, body, pageX, pageY;
-    var body = document.getElementById("body");
+console.log(audiolevel);
 
-    event = event || window.event; // IE-ism
 
-    // If pageX/Y aren't available and clientX/Y are,
-    // calculate pageX/Y - logic taken from jQuery.
-    // (This is to support old IE)
-    if (event.pageX == null && event.clientX != null) {
-      eventDoc = (event.target && event.target.ownerDocument) || document;
-      doc = eventDoc.documentElement;
-      body = eventDoc.body;
+// (function() {
+//   document.onmousemove = handleMouseMove;
+//   function handleMouseMove(event) {
+//     var dot, eventDoc, doc, body, pageX, pageY;
+//     var body = document.getElementById("body");
 
-      event.pageX =
-        event.clientX +
-        ((doc && doc.scrollLeft) || (body && body.scrollLeft) || 0) -
-        ((doc && doc.clientLeft) || (body && body.clientLeft) || 0);
-      event.pageY =
-        event.clientY +
-        ((doc && doc.scrollTop) || (body && body.scrollTop) || 0) -
-        ((doc && doc.clientTop) || (body && body.clientTop) || 0);
-    }
+//     event = event || window.event; // IE-ism
 
-    // Use event.pageX / event.pageY here
-    console.log("X : " + event.pageX);
+//     // If pageX/Y aren't available and clientX/Y are,
+//     // calculate pageX/Y - logic taken from jQuery.
+//     // (This is to support old IE)
+//     if (event.pageX == null && event.clientX != null) {
+//       eventDoc = (event.target && event.target.ownerDocument) || document;
+//       doc = eventDoc.documentElement;
+//       body = eventDoc.body;
 
-    if (event.pageX < 150) {
-      body.style.filter = "blur(20px)";
-    } else if (event.pageX < 300) {
-      body.style.filter = "blur(15px)";
-    } else if (event.pageX < 450) {
-      body.style.filter = "blur(10px)";
-    } else if (event.pageX < 600) {
-      body.style.filter = "blur(5px)";
-    } else if (event.pageX < 750) {
-      body.style.filter = "blur(2px)";
-      // } else if (event.pageX < 900) {
-      // body.style.filter = "blur(2px)";
-    } else if (event.pageX < 1150) {
-      body.style.filter = "blur(2px)";
-    } else if (event.pageX < 1300) {
-      body.style.filter = "blur(5px)";
-    } else if (event.pageX < 1550) {
-      body.style.filter = "blur(10px)";
-    } else if (event.pageX < 1700) {
-      body.style.filter = "blur(15px)";
-    } else if (event.pageX < 1850) {
-      body.style.filter = "blur(20px)";
-    }
-    if (event.pageX > 750 && event.pageX < 900) {
-      body.style.filter = "blur(0px)";
-    }
-  }
-})();
+//       event.pageX =
+//         event.clientX +
+//         ((doc && doc.scrollLeft) || (body && body.scrollLeft) || 0) -
+//         ((doc && doc.clientLeft) || (body && body.clientLeft) || 0);
+//       event.pageY =
+//         event.clientY +
+//         ((doc && doc.scrollTop) || (body && body.scrollTop) || 0) -
+//         ((doc && doc.clientTop) || (body && body.clientTop) || 0);
+//     }
+
+//     // Use event.pageX / event.pageY here
+//     console.log("X : " + event.pageX);
+
+//     if (event.pageX < 150) {
+//       body.style.filter = "blur(20px)";
+//     } else if (event.pageX < 300) {
+//       body.style.filter = "blur(15px)";
+//     } else if (event.pageX < 450) {
+//       body.style.filter = "blur(10px)";
+//     } else if (event.pageX < 600) {
+//       body.style.filter = "blur(5px)";
+//     } else if (event.pageX < 750) {
+//       body.style.filter = "blur(2px)";
+//       // } else if (event.pageX < 900) {
+//       // body.style.filter = "blur(2px)";
+//     } else if (event.pageX < 1150) {
+//       body.style.filter = "blur(2px)";
+//     } else if (event.pageX < 1300) {
+//       body.style.filter = "blur(5px)";
+//     } else if (event.pageX < 1550) {
+//       body.style.filter = "blur(10px)";
+//     } else if (event.pageX < 1700) {
+//       body.style.filter = "blur(15px)";
+//     } else if (event.pageX < 1850) {
+//       body.style.filter = "blur(20px)";
+//     }
+//     if (event.pageX > 750 && event.pageX < 900) {
+//       body.style.filter = "blur(0px)";
+//     }
+//   }
+// })();
---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 8080;
+
+http.listen(port);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/inc/views/index.html');
+});
+
+var five = require('johnny-five'); 
+var board = new five.Board();
+
+board.on('ready',function(){
+    var proximity = new five.Proximity({
+        controller: "HCSR04",
+        pin: 7
+    });
+
+    proximity.on("change", function() {
+        io.emit('audiolevel', this.cm);
+    });
+});
