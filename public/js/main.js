af8db547aaef91db1cfbda7c7caf585bd9d66c90
Mike Hendriks <info@mikehendriks.nl>
2019-01-09 21:18:49 +0100

    Dynamically add blur to background

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,26 @@
 (function() {
-    var body = document.getElementById("background");
-    var socket = io();
+    const background = document.getElementById("background");
+    const socket = io();
+    const treshhold = 32;
+    const double_treshhold = treshhold * 2;
     
     socket.on('distance', function(data) {
-      // console.log('distance: ' + data);
-      if (data > 150) {
-        body.style.filter = "blur(15px)";
-      } else if (data > 120) {
-        body.style.filter = "blur(10px)";
-      } else if (data > 100) {
-        body.style.filter = "blur(5px)";
-      } else if (data >= 80) {
-        body.style.filter = "blur(2px)";
-      } else if (data < 80) {
-        body.style.filter = "blur(0px)";
+      if (!(data > 75)) {
+        console.log('distance: ' + data);
+        if (data >= treshhold) {
+          let blur_amount = (data - treshhold) / 5;
+          background.style.filter = "blur(" + blur_amount + "px)";
+        } else if (data < treshhold) {
+          background.style.filter = "blur(0px)";
+        }
       }
     });
   
 })();
+
+// if (data >= treshhold) {
+//   let blur_amount = (data - treshhold) / 2;
+//   background.style.filter = "blur(" + blur_amount + "px)";
+// } else if (data < treshhold) {
+//   background.style.filter = "blur(0px)";
+// }
