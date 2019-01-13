773504f13a85cd4f12540248b39ad3fed16c4089
Mike Hendriks <info@mikehendriks.nl>
2019-01-14 00:06:43 +0100

    renamed shake to rumble, added earthquake class. Added tour dates, and made them shake randomly

--- public/js/main.js
+++ public/js/main.js
@@ -4,9 +4,9 @@ navigator.getUserMedia = navigator.getUserMedia ||
   navigator.mozGetUserMedia;
 if (navigator.getUserMedia) {
   navigator.getUserMedia({
-      audio: true
-    },
-    function(stream) {
+    audio: true
+  },
+    function (stream) {
       audioContext = new AudioContext();
       analyser = audioContext.createAnalyser();
       microphone = audioContext.createMediaStreamSource(stream);
@@ -21,42 +21,66 @@ if (navigator.getUserMedia) {
 
       // canvasContext = $("#canvas")[0].getContext("2d");
 
-      javascriptNode.onaudioprocess = function() {
-          var array = new Uint8Array(analyser.frequencyBinCount);
-          analyser.getByteFrequencyData(array);
-          var values = 0;
+      javascriptNode.onaudioprocess = function () {
+        var array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+        var values = 0;
 
-          var length = array.length;
-          for (var i = 0; i < length; i++) {
-            values += (array[i]);
-          }
-
-          var average = values / length;
-
-          console.log(Math.round(average));
-        
-        const logo = document.querySelector('.logo');
-        
-
-        if (average > 20) {
-          logo.classList.add('shake');
-        } else {
-          logo.classList.remove('shake');
+        var length = array.length;
+        for (var i = 0; i < length; i++) {
+          values += (array[i]);
         }
 
-          // canvasContext.clearRect(0, 0, 150, 300);
-          // canvasContext.fillStyle = '#BadA55';
-          // canvasContext.fillRect(0, 300 - average, 150, 300);
-          // canvasContext.fillStyle = '#262626';
-          // canvasContext.font = "48px impact";
-          // canvasContext.fillText(Math.round(average - 40), -2, 300);
+        var average = values / length;
+
+        console.log(Math.round(average));
+        
+        const logo = document.getElementById('logo');
+        const datesLength = document.getElementById('dates').childNodes.length;
+        const randomInt = Math.floor(Math.random() * datesLength);
+        const dates = document.getElementById('dates').childNodes;
+
+        
+        
+        if (average > 20) {
+          logo.className = 'earthquake';
+          dates[randomInt].className = "earthquake";
+        } else if (average > 15) {
+          logo.className = 'rumble3';
+          dates[randomInt].className = "rumble3";
+        } else if (average > 10) {
+          logo.className = 'rumble2';
+          dates[randomInt].className = "rumble2";
+        } else if (average > 5) {
+          logo.className = 'rumble';
+          dates[randomInt].className = "rumble";
+        } else {
+          logo.className = '';
+          dates[randomInt].className = '';
+        }
+
+        // canvasContext.clearRect(0, 0, 150, 300);
+        // canvasContext.fillStyle = '#BadA55';
+        // canvasContext.fillRect(0, 300 - average, 150, 300);
+        // canvasContext.fillStyle = '#262626';
+        // canvasContext.font = "48px impact";
+        // canvasContext.fillText(Math.round(average - 40), -2, 300);
         
 
-        } // end fn stream
+      } // end fn stream
     },
-    function(err) {
+    function (err) {
       console.log("The following error occured: " + err.name)
     });
 } else {
   console.log("getUserMedia not supported");
 }
+
+
+// function addClassToRandomElement(){
+//   var Elements = document.getElementsByClassName('date');
+//   var totalElements = Elements.length;
+//   var randomNumber = Math.floor(Math.random() * totalElements ) + 1;
+//   var randomElement = Elements[randomNumber];
+//   randomElement.classList.addClass('earthquake');
+// }
