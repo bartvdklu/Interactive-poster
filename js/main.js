137aab566820b16efcfcbd51dc80c6ed94a756f8
Bart van de Klundert <b.vdklundert@outlook.com>
2020-02-03 17:24:01 +0100

    Remove unnecessary files for the purpose of this project

--- js/main.js
+++ js/main.js
@@ -40,23 +40,3 @@ $(window).on("mousemove click", function(e) {
 });
 
 moveBackground();
-
-// (function() {
-//     const background = document.getElementById("background");
-//     const socket = io();
-//     const treshhold = 32;
-//     const double_treshhold = treshhold * 2;
-
-//     socket.on('distance', function(data) {
-//       if (!(data > 75)) {
-//         console.log('distance: ' + data);
-//         if (data >= treshhold) {
-//           let blur_amount = (data - treshhold) / 5;
-//           background.style.filter = "blur(" + blur_amount + "px)";
-//         } else if (data < treshhold) {
-//           background.style.filter = "blur(0px)";
-//         }
-//       }
-//     });
-
-// })();
