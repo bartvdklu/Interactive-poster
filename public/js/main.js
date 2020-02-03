a028d6faf7701867c6b692547493b1408990fc25
Bart van de Klundert <b.vdklundert@outlook.com>
2020-02-03 17:02:23 +0100

    Added Eindhoven

--- public/js/main.js
+++ public/js/main.js
@@ -1,38 +1,42 @@
 var lFollowX = 0,
-    lFollowY = 0,
-    x = 0,
-    y = 0,
-    friction = 1 / 30;
+  lFollowY = 0,
+  x = 0,
+  y = 0,
+  friction = 1 / 30;
 
 function moveBackground() {
   x += (lFollowX - x) * friction;
   y += (lFollowY - y) * friction;
-  
-  translate = 'translate(' + x / 2+ 'px, ' + y / 2 + 'px)';
-  translateNegative = 'translate(' + -x + 'px, ' + -y + 'px) scale(1.1)';
 
-  $('#background').css({
-    '-webit-transform': translateNegative,
-    '-moz-transform': translateNegative,
-    'transform': translateNegative
+  translate = "translate(" + x / 2 + "px, " + y / 2 + "px)";
+  translateNegative = "translate(" + -x + "px, " + -y + "px) scale(1.1)";
+
+  $(".BG").css({
+    "-webit-transform": translateNegative,
+    "-moz-transform": translateNegative,
+    transform: translateNegative
   });
 
-  $('#foreground').css({
-    '-webit-transform': translate,
-    '-moz-transform': translate,
-    'transform': translate
+  $(".FG").css({
+    "-webit-transform": translate,
+    "-moz-transform": translate,
+    transform: translate
   });
 
   window.requestAnimationFrame(moveBackground);
 }
 
-$(window).on('mousemove click', function(e) {
-
-  var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
-  var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+$(window).on("mousemove click", function(e) {
+  var lMouseX = Math.max(
+    -100,
+    Math.min(100, $(window).width() / 2 - e.clientX)
+  );
+  var lMouseY = Math.max(
+    -100,
+    Math.min(100, $(window).height() / 2 - e.clientY)
+  );
   lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
   lFollowY = (10 * lMouseY) / 100;
-
 });
 
 moveBackground();
@@ -42,7 +46,7 @@ moveBackground();
 //     const socket = io();
 //     const treshhold = 32;
 //     const double_treshhold = treshhold * 2;
-    
+
 //     socket.on('distance', function(data) {
 //       if (!(data > 75)) {
 //         console.log('distance: ' + data);
@@ -54,5 +58,5 @@ moveBackground();
 //         }
 //       }
 //     });
-  
+
 // })();
